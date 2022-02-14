00e24124a8e1191efcebf49481116642b51c640f
polarbear23 <xkyd96@gmail.com>
2022-02-14 17:28:47 +0000

    added responses and verification and authentication

--- src/router.js
+++ src/router.js
@@ -14,11 +14,29 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (username === mockUser.username) {
+        if (password === mockUser.password) {
+            const payload = { username: mockUser.username }
+            const token = jwt.sign(payload, "bob");
+            return res.json({ data: token });
+        }
+    }
+    return res.status(400).send({
+        message: 'username or password incorrect!'
+    });
 
 });
 
 router.get('/profile', (req, res) => {
-
+    console.log(req.headers.authentication);
+    try {
+        jwt.verify(req.headers.authentication, "bob")
+        return res.json(mockUser.profile);
+    }
+    catch {
+        return res.json("invalid token");
+    }
 });
 
 module.exports = router;
